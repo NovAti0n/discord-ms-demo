82ca7df7f0f129f197fc0fdf846002ae74986694
Noa Quenon <noa.quenon@proton.me>
2024-02-22 21:52:43 +0100

    Add Microsoft OAuth integration

--- src/microsoft.js
+++ src/microsoft.js
@@ -6,25 +6,25 @@ import config from "./config.js";
 export function getOAuthUrl() {
 	const state = crypto.randomUUID();
 
-	const url = new URL("https://discord.com/api/oauth2/authorize");
-	url.searchParams.set("client_id", config.DISCORD_CLIENT_ID);
-	url.searchParams.set("redirect_uri", config.DISCORD_REDIRECT_URI);
+	const url = new URL(`https://login.microsoftonline.com/${config.MICROSOFT_TENANT_ID}/oauth2/v2.0/authorize`);
+	url.searchParams.set("client_id", config.MICROSOFT_CLIENT_ID);
+	url.searchParams.set("redirect_uri", config.MICROSOFT_REDIRECT_URI);
 	url.searchParams.set("response_type", "code");
 	url.searchParams.set("state", state);
-	url.searchParams.set("scope", "identify");
+	url.searchParams.set("scope", "openid");
 	url.searchParams.set("prompt", "consent");
 
 	return { state, url: url.toString() };
 }
 
 export async function getOAuthToken(code) {
-	const url = "https://discord.com/api/v10/oauth2/token";
+	const url = `https://login.microsoftonline.com/${config.MICROSOFT_TENANT_ID}/oauth2/v2.0/token`;
 	const body = new URLSearchParams({
-		client_id: config.DISCORD_CLIENT_ID,
-		client_secret: config.DISCORD_CLIENT_SECRET,
+		client_id: config.MICROSOFT_CLIENT_ID,
+		client_secret: config.MICROSOFT_CLIENT_SECRET,
 		grant_type: "authorization_code",
 		code,
-		redirect_uri: config.DISCORD_REDIRECT_URI
+		redirect_uri: config.MICROSOFT_REDIRECT_URI
 	});
 
 	const response = await fetch(url, {
@@ -38,11 +38,11 @@ export async function getOAuthToken(code) {
 	if (response.ok) {
 		const data = await response.json();
 		return data;
-	} else throw new Error(`Unable to fetch Discord OAuth token: [${response.status}] ${response.statusText}`);
+	} else throw new Error(`Unable to fetch Microsoft OAuth token: [${response.status}] ${response.statusText}`);
 }
 
 export async function getUserData(token) {
-	const url = "https://discord.com/api/v10/oauth2/@me";
+	const url = `https://graph.microsoft.com/oidc/userinfo`;
 
 	const response = await fetch(url, {
 		headers: {
@@ -53,5 +53,5 @@ export async function getUserData(token) {
 	if (response.ok) {
 		const data = await response.json();
 		return data;
-	} else throw new Error(`Unable to fetch Discord user data: [${response.status}] ${response.statusText}`);
+	} else throw new Error(`Unable to fetch Microsoft user data: [${response.status}] ${response.statusText}`);
 }
--- src/server.js
+++ src/server.js
@@ -3,12 +3,13 @@ import express from "express";
 
 import config from "./config.js";
 import * as discord from "./discord.js";
+import * as microsoft from "./microsoft.js";
 
 const app = express();
-app.use(cookieParser(config.DISCORD_COOKIE_SECRET)); // Used for signing cookies
+app.use(cookieParser(config.COOKIE_SECRET)); // Used for signing cookies
 
 app.get("/", (_, res) => {
-	res.send("🫡");
+	res.send("Hello World!");
 });
 
 app.get("/verify", async (_, res) => {
@@ -30,14 +31,38 @@ app.get("/discord-callback", async (req, res) => {
 			return res.sendStatus(403);
 		}
 
-		console.log("State validation successful");
-
 		const token = await discord.getOAuthToken(code);
 		const userData = await discord.getUserData(token);
+		const { state, url } = microsoft.getOAuthUrl();
 
-		console.log(`User ID is ${userData.user.id}`);
+		console.log(`Discord user ID is ${userData.user.id}`);
 
-		res.send(`Hello ${userData.user.username}`);
+		res.cookie("clientState", state, { maxAge: 1000 * 60 * 10, signed: true });
+		res.redirect(url);
+	} catch (err) {
+		console.error(err);
+		res.sendStatus(500);
+	}
+});
+
+app.get("/microsoft-callback", async (req, res) => {
+	try {
+		const code = req.query.code;
+		const microsoftState = req.query.state;
+		const { clientState } = req.signedCookies;
+
+		if (clientState != microsoftState) {
+			console.error("States don't match, aborting...");
+
+			return res.sendStatus(403);
+		}
+
+		const token = await microsoft.getOAuthToken(code);
+		const userData = await microsoft.getUserData(token);
+
+		console.log(`Microsoft user ID is ${userData.sub}`);
+
+		res.send("Success");
 	} catch (err) {
 		console.error(err);
 		res.sendStatus(500);
