e5f505a182b6c38eaad50d11c6bf57d91c14f5c0
Noa Quenon <noa.quenon@proton.me>
2024-02-23 11:32:34 +0100

    Fix .env file

--- server/src/config.js
+++ server/src/config.js
@@ -1,9 +1,8 @@
 import * as dotenv from "dotenv";
 
-dotenv.config();
+dotenv.config({ path: "../.env" });
 
 const config = {
-	DISCORD_TOKEN: process.env.DISCORD_TOKEN,
 	DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
 	DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
 	DISCORD_REDIRECT_URI: process.env.DISCORD_REDIRECT_URI,
